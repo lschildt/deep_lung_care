1a27b1761fbc4fea80a6b9d5bc09d063c5b5d4f0
Leandro Schildt <leandro.schildt@MacBook-Pro-de-Leandro.local>
2021-05-30 20:41:19 -0300

    Added scroll on the exercice instruction screens

--- lib/widgets/ExerciseInstructionScreen.dart
+++ lib/widgets/ExerciseInstructionScreen.dart
@@ -25,48 +25,50 @@ class ExerciseInstructionScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body:Column(
-        children: <Widget>[
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.all(10.0),
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      color: Colors.white,
-                    ),
-                    child: Image.asset('assets/images/' + imgAnimation, height: 200),
-                )
-              ]
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.all(10.0),
-                    padding: const EdgeInsets.all(3.0),
-                    child: Image.asset('assets/images/' + imgInstructions, height: 140)
-                )
-              ]
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.all(10.0),
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      color: Colors.white,
-                    ),
-                    child: Text(description
-                        , style: TextStyle(fontSize: descriptionFontSize, fontWeight: FontWeight.bold, color: Colors.black))
-                )
-              ]
-          ),
-        ]
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        color: Colors.white,
+                      ),
+                      child: Image.asset('assets/images/' + imgAnimation, height: 200),
+                  )
+                ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(3.0),
+                      child: Image.asset('assets/images/' + imgInstructions, height: 140)
+                  )
+                ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        color: Colors.white,
+                      ),
+                      child: Text(description
+                          , style: TextStyle(fontSize: descriptionFontSize, fontWeight: FontWeight.bold, color: Colors.black))
+                  )
+                ]
+            ),
+          ]
+        )
       )
     );
   }

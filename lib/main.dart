8d5cd07c30e1f25975f343dde88179b053615c42
Leandro Schildt <leandro.schildt@MacBook-Pro-de-Leandro.local>
2021-06-03 17:15:59 -0300

    Adjust to autoresize on main screen

--- lib/main.dart
+++ lib/main.dart
@@ -50,24 +50,27 @@ class _MyHomePageState extends State<MyHomePage> {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              CarouselSlider(
-                  options: CarouselOptions(
-                    height: 170.0,
-                    viewportFraction: 1.0,
-                    enlargeCenterPage: false,
-                    autoPlay: true,
-                    autoPlayInterval: Duration(seconds: 5),
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    pauseAutoPlayOnTouch: true,
-                    aspectRatio: 5.0,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _currentIndex = index;
-                      });
-                    },
+              Container(
+                child:
+                  CarouselSlider(
+                      options: CarouselOptions(
+                        height: 160.0,
+                        viewportFraction: 1.0,
+                        enlargeCenterPage: false,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 5),
+                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        pauseAutoPlayOnTouch: true,
+                        aspectRatio: 5.0,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _currentIndex = index;
+                          });
+                        },
+                      ),
+                      items: imageSliders
                   ),
-                  items: imageSliders
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -101,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           child: Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
                             child: Center (
-                              child : Text("FISIOTERAPIA RESPIRATÓRIA", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green))
+                              child : Text("FISIOTERAPIA RESPIRATÓRIA", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green))
                             ),
                           ),
                         ),
@@ -109,105 +112,81 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ]
               ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset('assets/images/logo.webp', width: 90),
-                    SizedBox(width: 20),
-                    Container(
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(
-                          minWidth: 80.0,
-                          maxWidth: 240.0,
-                          minHeight: 100.0,
-                          maxHeight: 180.0,
-                        ),
-                        child: AutoSizeText(
-                          "Plataforma digital desenvolvida para auxiliar você à realizar exercícios ventilatórios de forma moderna e didática.",
-                          style: TextStyle(fontSize: 14, color: Colors.green),
+              Container(
+                margin: const EdgeInsets.all(5.0),
+                padding: const EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          margin: const EdgeInsets.all(5.0),
+                          padding: const EdgeInsets.all(5.0),
+                          child: Image.asset('assets/images/logo.webp', width: 90),
                         ),
                       ),
-                    )
-
-                    /*Card(
-                      elevation: 0,
-                      shadowColor: Colors.black,
-                      color: Colors.white,
-                      child: InkWell(
-                        splashColor: Colors.green.withAlpha(30),
-                        onTap: () {
-                          print('Card tapped.');
-                        },
-                        child: SizedBox(
-                          width: (MediaQuery.of(context).size.width) - 10,
-                          height: 140,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Image.asset('assets/images/logo.webp', height: 70, width: 90),
-                                    SizedBox(width: 20),
-                                    Container(
-                                      child:
-                                       /* Text("Plataforma digital desenvolvida para\nauxiliar você à realizar exercícios\nventilatórios de forma moderna e didática."
-                                            , style: TextStyle(fontSize: 14, color: Colors.green)),*/
-                                        AutoSizeText('Plataforma digital desenvolvida para auxiliar você à realizar exercícios ventilatórios de forma moderna e didática.'),
-
-
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
+                      Expanded (
+                        flex: 4,
+                        child:
+                        Container(
+                          margin: const EdgeInsets.all(5.0),
+                          padding: const EdgeInsets.all(5.0),
+                          child: AutoSizeText(
+                            "Plataforma digital desenvolvida para auxiliar você à realizar exercícios ventilatórios de forma moderna e didática.",
+                            style: TextStyle(fontSize: 14, color: Colors.green),
                           ),
                         ),
-                      ),
-                    ),*/
-                  ]
+                      )
+                    ]
+                ),
               ),
-              Row(
+              Container(
+                margin: const EdgeInsets.all(5.0),
+                padding: const EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Card(
-                      elevation: 0,
-                      shadowColor: Colors.black,
-                      color: Colors.white,
-                      child: InkWell(
-                        splashColor: Colors.green.withAlpha(30),
-                        onTap: () {
-                          print('Card tapped.');
-                        },
-                        child: SizedBox(
-                          width: (MediaQuery.of(context).size.width) - 10,
-                          height: 180,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-                            child: Row(
-                              children: <Widget>[
-                                Text("DEEP CARE", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green)),
-                                SizedBox(width: 80),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    Text("PARE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                                    SizedBox(height: 8),
-                                    Text("RESPIRE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                                    SizedBox(height: 8),
-                                    Text("PRATIQUE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                                  ],
-                                ),
-                                Spacer(),
-                                Image.asset('assets/images/deep.png', height: 120),
-                              ],
-                            ),
-                          ),
-                        ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        child: Text("DEEP CARE", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green)),
                       ),
                     ),
-                  ]
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                          children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text("PARE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 8),
+                                  Text("RESPIRE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 8),
+                                  Text("PRATIQUE", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 90,
+                              child: Image.asset('assets/images/deep.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           )
